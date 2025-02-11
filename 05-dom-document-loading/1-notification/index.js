d28c6f4e933492b89b140d43ff8ac0b2ef2ebfb8
tarashev <e.tarashev@yandex.ru>
2025-02-11 20:21:19 +0500

    5.1 notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
+  static #lastShownComponent;
+
+  constructor(message, {
+    duration = 1000,
+    type = 'success',
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  show(target = document.body) {
+    if (NotificationMessage.lastShownComponent) {
+      NotificationMessage.lastShownComponent.hide();
+    }
+
+    NotificationMessage.lastShownComponent = this;
+
+    target.append(this.element);
+
+    this.timerId = setTimeout(() => {
+      this.hide();
+    }, this.duration);
+  }
+
+  hide() {
+    this.destroy();
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+
+  createTemplate() {
+    return `<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+              <div class="timer"></div>
+              <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                 ${this.message}
+               </div>
+              </div>
+           </div>`;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    clearTimeout(this.timerId);
+  }
 
 }
